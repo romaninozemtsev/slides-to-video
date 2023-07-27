4c45b37529410f90ef716c5c49c97e187213a7aa
Roman Inozemtsev <roman.inozemtsev+github@gmail.com>
2023-07-27 00:42:25 -0700

    adding splitting into many files

--- src/civic_tests.py
+++ src/civic_tests.py
@@ -4,6 +4,17 @@ import json
 from ssml_helper import voice_tag, speak_tag, paragraph_tag, break_tag
 from text_to_speech_azure import speech_synthesis_bookmark_event
 
+
+def wrap_saml(voice, saml: str|list[str]):
+    result = []
+    with speak_tag(result):
+        with voice_tag(voice, result):
+            if isinstance(saml, list):
+                result.extend(saml)
+            else:
+                result.append(saml)
+    return "\n".join(result)
+
 if __name__ == '__main__':
     voice = 'en-US-SaraNeural'
     if len(sys.argv) > 1:
@@ -22,23 +33,36 @@ if __name__ == '__main__':
     # each question has "question" and "answer"
 
     result = []
+    limit = 10
     with speak_tag(result):
         for section in sections:
             section_name = section["section"]
             questions = section["questions"]                
-            with voice_tag(voice, result):
-                result.append(paragraph_tag(section_name))
+            # with voice_tag(voice, result):
+            #     result.append(paragraph_tag(section_name))
 
             for question in questions:
-                with voice_tag(voice, result):
-                    question_name = question["question"]
-                    result.append(paragraph_tag(question_name))
-                    result.append(break_tag('1s'))
-                with voice_tag(voice2, result):
-                    answers = question["answer"]
-                    for answer in answers:
-                        result.append(paragraph_tag(answer))
+                q_saml = []
+                a_saml = []
+                with speak_tag(q_saml):
+                    with voice_tag(voice, q_saml):
+                        question_name = question["question"]
+                        q_saml.append(paragraph_tag(question_name))
+                        q_saml.append(break_tag('1s'))
+                with speak_tag(a_saml):
+                    with voice_tag(voice2, a_saml):
+                        answers = question["answer"]
+                        for answer in answers:
+                            a_saml.append(paragraph_tag(answer))
+                with open(f"out/civic-test/saml/q{question['number']}.txt", 'w') as f:
+                    f.write("\n".join(q_saml))
+                with open(f"out/civic-test/saml/a{question['number']}.txt", 'w') as f:
+                    f.write("\n".join(a_saml))
+                
 
     saml = "\n".join(result)
 
-    speech_synthesis_bookmark_event(saml, 'out/civic-test.mp3', capture_bookmarks=False, capture_words=False)
+
+    q_saml_str = "\n".join(q_saml)
+
+    speech_synthesis_bookmark_event(q_saml_str, 'out/civic-test/civic-test.wav', capture_bookmarks=False, capture_words=False)
